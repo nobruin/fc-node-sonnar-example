f3ec4fb84dc90978a9711ec9a767789c77762795
ext_brsalis <ext_brsalis@mercadolibre.com>
2023-09-26 14:18:36 +0000

    feature/create-tests: add tests of calc function

---
+++ src/calc.spec.js
@@ -0,0 +1,24 @@
+const calc = require("./calc");
+
+describe("Calc unit tests", () => {
+
+    it("Should be 4 when sum 1 and 3", () =>{
+        const result = calc.sum(1,3)
+        expect(result).toBe(4)
+    });
+    
+    it("Should be 4 when multiple of 2 of 2", () =>{
+        const result = calc.multi(2,2)
+        expect(result).toBe(4)
+    });
+
+    it("Should be 4 when we subtract 2 from 6", () =>{
+        const result = calc.subtraction(6,2)
+        expect(result).toBe(4)
+    });
+
+    it("Should be 4 when divide 8 by 2", () =>{
+        const result = calc.subtraction(6,2)
+        expect(result).toBe(4)
+    });
+})
